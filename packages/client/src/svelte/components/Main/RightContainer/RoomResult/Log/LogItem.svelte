72f8ec690bc82a59d0b6caa6b3fec95623be0e51
manegame <contact@manusnijhoff.nl>
2025-03-21 17:58:05 +0100

    fix: animated logs

--- packages/client/src/svelte/components/Main/RightContainer/RoomResult/Log/LogItem.svelte
+++ packages/client/src/svelte/components/Main/RightContainer/RoomResult/Log/LogItem.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
   import type { LogEntry } from "@components/Main/RightContainer/RoomResult/types"
+  import { fade } from "svelte/transition"
+  import { playSound } from "@modules/sound"
 
-  export let logEntry: LogEntry
-  export let i: number
-  console.log(i)
+  let { logEntry, i }: { logEntry: LogEntry; i: number } = $props()
+
+  const parseTimestamp = str => {
+    let datetime = new Date("1970-01-01T00:" + str + "Z").getSeconds()
+    console.log(datetime, "millis")
+    return datetime
+  }
+
+  let millis = $derived(parseTimestamp(logEntry.timestamp))
+
+  $effect(() => {
+    setTimeout(() => playSound("tcm", "selectionScroll"), millis * 100)
+  })
 </script>
 
-<div class="log-entry">
-  <span class="timestamp">{logEntry.timestamp}</span> –
-  <span class="event">{logEntry.event}</span>
-</div>
+{#if i > -1}
+  <div in:fade|global={{ delay: millis * 100 }} class="log-entry">
+    <span class="timestamp">{logEntry.timestamp}</span> –
+    <span class="event">{logEntry.event}</span>
+  </div>
+{/if}
 
 <style lang="scss">
   .log-entry {
