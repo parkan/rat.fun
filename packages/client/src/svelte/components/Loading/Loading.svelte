eccc89df7e724cadbe2a7a0f130b3a2948ba34f7
PWR <pwrpntr@gmail.com>
2025-04-10 15:10:15 +0200

    basic loading screen

--- packages/client/src/svelte/components/Loading/Loading.svelte
+++ packages/client/src/svelte/components/Loading/Loading.svelte
@@ -13,7 +13,12 @@
 
   // Finished when chain is ready
   $: if ($ready) {
+    initSequence()
+  }
+
+  const initSequence = async () => {
     initEntities()
+    await new Promise(resolve => setTimeout(resolve, 3000))
     done()
   }
 
@@ -23,11 +28,37 @@
 </script>
 
 <div class="loading" class:done={Number($loadingMessage) === 100}>
-  <div>{$loadingMessage}</div>
+  <div class="inner">
+    <img src="/images/logo.png" alt="logo" />
+    <div class="message">
+      <span class="highlight">{$loadingMessage}</span>
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
   .loading {
     text-align: center;
+
+    .inner {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+      img {
+        width: 200px;
+      }
+
+      .message {
+        margin-top: 10px;
+
+        .highlight {
+          background: var(--color-value);
+          color: black;
+          padding: 5px;
+        }
+      }
+    }
   }
 </style>
