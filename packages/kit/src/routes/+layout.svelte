56cd6ef551697e3ab5df3365c4ac251f50972b5c
manegame <contact@manusnijhoff.nl>
2025-06-19 11:53:23 +0200

    feat: basic scaffolding of layout

---
+++ packages/kit/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import "../app.css"
+  import type { LayoutProps } from './$types';
+
+  let { children, data }: LayoutProps = $props()
+
+  $inspect(data.environment)
+</script>
+
+{@render children?.()}
---
+++ packages/kit/src/routes/+layout.ts
@@ -0,0 +1,27 @@
+import type { LayoutLoad } from './$types';
+import { ENVIRONMENT } from "$lib/mud/enums"
+
+export const prerender = false
+export const ssr = false
+
+const getEnvironment = (url: URL) => {
+  const hostname = url.hostname
+
+  if (hostname.includes("rhodolite") || url.searchParams.has("rhodolite")) {
+    return ENVIRONMENT.RHODOLITE
+  }
+
+  if (hostname.includes("pyrope") || url.searchParams.has("pyrope")) {
+    return ENVIRONMENT.PYROPE
+  }
+
+  return ENVIRONMENT.DEVELOPMENT
+}
+
+export const load: LayoutLoad = async ({ url }) => {
+  const environment = getEnvironment(url)
+
+  return {
+    environment: ENVIRONMENT[environment]
+  }
+}
