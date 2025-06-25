e75d1bd4726c39406a8a30d8ed2b29ce575dc169
PWR <pwrpntr@gmail.com>
2025-06-25 18:26:05 +0200

    server: use recoverMessageAddress

--- packages/server/src/modules/signature/index.ts
+++ packages/server/src/modules/signature/index.ts
@@ -1,24 +1,12 @@
-import { toBuffer, fromRpcSig, ecrecover, pubToAddress, bufferToHex } from "ethereumjs-util"
-import jsSha3 from "js-sha3"
+import type { Hex } from "viem"
 import { addressToId } from "@modules/signature/utils"
 import { OFFCHAIN_VALIDATION_MESSAGE } from "@config"
+import { recoverMessageAddress } from "viem"
 
-export function getSenderId(signature: string) {
-  const keccak256 = jsSha3.keccak256
-
-  // Prefix the message per Ethereum's signing process
-  const prefix = `\u0019Ethereum Signed Message:\n${OFFCHAIN_VALIDATION_MESSAGE.length}`
-  const prefixedMessage = prefix + OFFCHAIN_VALIDATION_MESSAGE
-
-  // Hash the prefixed message and ensure it's a 0x-prefixed hex string
-  const messageHashHex = "0x" + keccak256(prefixedMessage)
-  const messageHash = toBuffer(messageHashHex)
-
-  // Recover the address from the signature
-  const { v, r, s } = fromRpcSig(signature)
-  const pub = ecrecover(messageHash, v, r, s)
-  const addrBuf = pubToAddress(pub)
-  const recoveredAddress = bufferToHex(addrBuf)
-
+export async function getSenderId(signature: Hex) {
+  const recoveredAddress = await recoverMessageAddress({
+    message: OFFCHAIN_VALIDATION_MESSAGE,
+    signature
+  })
   return addressToId(recoveredAddress)
 }
