72051f7b70c24f1aeac38b6f63e89f3aa0223381
PWR <pwrpntr@gmail.com>
2025-07-03 12:07:02 +0200

    edit to vite config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -7,8 +7,6 @@ export default defineConfig({
   define: {
     // Fix for RAINBOW_PROVIDER_API_KEY error
     // https://github.com/rainbow-me/rainbowkit/discussions/2003
-    "process.env.RAINBOW_PROVIDER_API_KEY": JSON.stringify(
-      process.env.RAINBOW_PROVIDER_API_KEY || ""
-    )
+    "process.env.RAINBOW_PROVIDER_API_KEY": JSON.stringify("")
   }
 })
