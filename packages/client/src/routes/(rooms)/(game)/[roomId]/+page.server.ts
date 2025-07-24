32faa27bc1c76c3f058b55eb49eef597131948a8
manegame <contact@manusnijhoff.nl>
2025-07-24 16:36:55 +0200

    fix: no more ssr

--- packages/client/src/routes/(rooms)/(game)/[roomId]/+page.server.ts
+++ packages/client/src/routes/(rooms)/(game)/[roomId]/+page.server.ts
@@ -4,7 +4,7 @@ import { queries } from "$lib/modules/content/sanity/groq"
 import { error, redirect } from "@sveltejs/kit"
 import { CMSError } from "$lib/modules/error-handling"
 
-export const ssr = true
+export const ssr = false
 
 export const load: PageLoad = async ({ params }) => {
   try {
--- packages/client/src/routes/+layout.server.ts
+++ packages/client/src/routes/+layout.server.ts
@@ -1,7 +1,7 @@
 import type { LayoutLoad } from "./$types"
 import { getEnvironment, getWalletType } from "$lib/modules/network"
 
-export const ssr = true
+export const ssr = false
 
 export const load: LayoutLoad = async ({ url }) => {
   const environment = getEnvironment(url)
