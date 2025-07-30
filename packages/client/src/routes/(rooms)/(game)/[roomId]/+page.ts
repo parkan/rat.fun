d97b2b74a1d326b3738c367c982d490f692160f1
manegame <contact@manusnijhoff.nl>
2025-07-30 17:23:05 +0200

    fix: url image problems

--- packages/client/src/routes/(rooms)/(game)/[roomId]/+page.ts
+++ packages/client/src/routes/(rooms)/(game)/[roomId]/+page.ts
@@ -2,6 +2,7 @@ import type { PageLoad } from "./$types"
 import { loadData } from "$lib/modules/content/sanity"
 import { queries } from "$lib/modules/content/sanity/groq"
 import { redirect } from "@sveltejs/kit"
+import { errorHandler, CMSError } from "$lib/modules/error-handling"
 
 export const load: PageLoad = async ({ params }) => {
   try {
@@ -12,6 +13,7 @@ export const load: PageLoad = async ({ params }) => {
     }
   } catch (err) {
     console.error(err)
+    errorHandler(new CMSError("Could not load data"))
     redirect(302, "/")
   }
 }
