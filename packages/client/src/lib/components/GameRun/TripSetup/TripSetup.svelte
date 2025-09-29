5ff0b4f1635d170e46417b90f6bfb7468298e43c
PWR <pwrpntr@gmail.com>
2025-09-29 16:36:36 +0200

    client: rework trip setup

--- packages/client/src/lib/components/GameRun/TripSetup/TripSetup.svelte
+++ packages/client/src/lib/components/GameRun/TripSetup/TripSetup.svelte
@@ -3,15 +3,12 @@
   import { gsap } from "gsap"
   import { TextPlugin } from "gsap/TextPlugin"
   import type { Room as SanityRoom } from "@sanity-types"
-
-  import LogoBox from "./Boxes/LogoBox.svelte"
-  import TextLogBox from "./Boxes/TextLogBox.svelte"
-  import TripPromptBox from "./Boxes/TripPromptBox.svelte"
-  import VideoFeedBox from "./Boxes/VideoFeedBox.svelte"
-  import DataFeedBox from "./Boxes/DataFeedBox.svelte"
-  import BrainScanBox from "./Boxes/BrainScanBox.svelte"
+  import { terminalTyper } from "$lib/modules/terminal-typer/index"
+  import { generateTripSetupOutput } from "./tripSetupOutput"
 
   gsap.registerPlugin(TextPlugin)
+
+  let terminalBoxElement = $state<HTMLDivElement>()
 
   const {
     onComplete,
@@ -23,41 +20,31 @@
 
   const SETUP_DURATION = 5000
 
+  // const text = [
+  //   "= Pneumatic system engaged",
+  //   `= Rat ${$rat.name} conveyed to trip chamber`,
+  //   "= Trip chamber lid closed",
+  //   "= RAT-o-FUN harness secured",
+  //   "= PETA approved Diaper attached",
+  //   "= Intercranial probes attached",
+  //   "= Rat dosed with 44mg of Slopamine",
+  //   "= Video feed activated",
+  //   "= Trip initiated....."
+  // ]
+
   onMount(async () => {
     setTimeout(() => {
       onComplete()
     }, SETUP_DURATION)
+
+    if (terminalBoxElement) {
+      await terminalTyper(terminalBoxElement, generateTripSetupOutput())
+    }
   })
 </script>
 
 <div class="splash-screen">
-  <div class="inner">
-    <!-- GROUP 1 -->
-    <div class="box-group-1">
-      <div class="box-group-1-slot-1">
-        <LogoBox />
-      </div>
-      <div class="box-group-1-slot-2">
-        <TextLogBox />
-      </div>
-    </div>
-    <!-- GROUP 2 -->
-    <div class="box-group-2">
-      <VideoFeedBox />
-    </div>
-    <!-- GROUP 3 -->
-    <div class="box-group-3">
-      <div class="box-group-3-slot-1">
-        <TripPromptBox {staticRoomContent} />
-      </div>
-      <div class="box-group-3-slot-2">
-        <BrainScanBox />
-      </div>
-      <div class="box-group-3-slot-3">
-        <DataFeedBox />
-      </div>
-    </div>
-  </div>
+  <div class="terminal-box" bind:this={terminalBoxElement}></div>
 </div>
 
 <style lang="scss">
@@ -72,63 +59,10 @@
     font-size: var(--font-size-normal);
     background: rgba(0, 0, 0, 0.1);
 
-    .inner {
-      display: flex;
-      flex-wrap: wrap;
+    .terminal-box {
       font-size: var(--font-size-normal);
       width: calc(100% - 30px);
       height: calc(100% - 40px);
-      border: 5px double #444444;
-
-      .box-group-1 {
-        display: flex;
-        flex-direction: column;
-        width: 50%;
-        height: 50%;
-
-        .box-group-1-slot-1 {
-          width: 100%;
-          height: 100px;
-          border-bottom: 5px double #444444;
-        }
-
-        .box-group-1-slot-2 {
-          width: 100%;
-          height: calc(100% - 100px);
-          border-bottom: 5px double #444444;
-        }
-      }
-
-      .box-group-2 {
-        display: flex;
-        flex-direction: column;
-        width: 50%;
-        height: 50%;
-        border-bottom: 5px double #444444;
-        border-left: 5px double #444444;
-      }
-
-      .box-group-3 {
-        display: flex;
-        flex-direction: row;
-        width: 100%;
-        height: 50%;
-
-        .box-group-3-slot-1 {
-          width: 33.33%;
-          height: 100%;
-          border-right: 5px double #444444;
-        }
-        .box-group-3-slot-2 {
-          width: 33.33%;
-          height: 100%;
-          border-right: 5px double #444444;
-        }
-        .box-group-3-slot-3 {
-          width: 33.33%;
-          height: 100%;
-        }
-      }
     }
   }
 </style>
---
+++ packages/client/src/lib/components/GameRun/TripSetup/tripSetupOutput.ts
@@ -0,0 +1,129 @@
+import type { TerminalOutputUnit } from "$lib/modules/terminal-typer/types"
+
+export function generateTripSetupOutput() {
+  const tripSetupOutput: TerminalOutputUnit[] = [
+    {
+      type: "text",
+      content: "RAT.FUN SCIENTIFIC INSTRUMENTS LLC",
+      color: "white",
+      backgroundColor: "black",
+      duration: 0,
+      delayAfter: 50
+    },
+    {
+      type: "text",
+      content: "(c) 2034 Sexc-hell island, Kowloon, Hong Kong",
+      color: "white",
+      backgroundColor: "black",
+      duration: 0,
+      delayAfter: 50
+    },
+    {
+      type: "text",
+      content: "All Rights Reserved in accordance with Maritime Law",
+      color: "white",
+      backgroundColor: "black",
+      duration: 0,
+      delayAfter: 50
+    },
+    {
+      type: "text",
+      content: "Patent pending in all non-aligned territories",
+      color: "white",
+      backgroundColor: "black",
+      duration: 0,
+      delayAfter: 50
+    },
+    {
+      type: "text",
+      content: "FUN and RAT are registered trademarks of RAT.FUN SCIENTIFIC INSTRUMENTS LLC",
+      color: "white",
+      backgroundColor: "black",
+      duration: 0,
+      delayAfter: 50
+    },
+    {
+      type: "text",
+      content:
+        "FUN HEREBY SHOULD BE INFERNALLY INTERPRETED AS THE SKILL FULL APPLICATION OF KNOWLEDGE AND NON-STOCHASTIC",
+      color: "white",
+      backgroundColor: "black",
+      duration: 0,
+      delayAfter: 50
+    },
+    {
+      type: "text",
+      content: 'PROCESSES (ie. "GOD MODE incarnate")',
+      color: "white",
+      backgroundColor: "black",
+      duration: 0,
+      delayAfter: 50
+    },
+    {
+      type: "text",
+      content: "Setting up trip...",
+      color: "white",
+      backgroundColor: "red",
+      delayAfter: 100
+    },
+    {
+      type: "loader",
+      content: "Loading...",
+      color: "white",
+      duration: 500,
+      backgroundColor: "red",
+      loaderCharacters: "#"
+    },
+    {
+      type: "text",
+      content: "= Pneumatic system engaged",
+      color: "white",
+      backgroundColor: "black",
+      delayAfter: 100
+    },
+    {
+      type: "text",
+      content: "= Rat conveyed to trip chamber",
+      color: "white",
+      backgroundColor: "black",
+      delayAfter: 100
+    },
+    {
+      type: "text",
+      content: "= PETA approved Diaper attached",
+      color: "white",
+      backgroundColor: "black",
+      delayAfter: 100
+    },
+    {
+      type: "text",
+      content: "= Intercranial probes attached",
+      color: "white",
+      backgroundColor: "black",
+      delayAfter: 100
+    },
+    {
+      type: "text",
+      content: "= Rat dosed with 44mg of Slopamine",
+      color: "white",
+      backgroundColor: "black",
+      delayAfter: 100
+    },
+    {
+      type: "text",
+      content: "= Trip initiated.....",
+      color: "white",
+      backgroundColor: "black",
+      delayAfter: 100
+    },
+    {
+      type: "loader",
+      content: "",
+      color: "orange",
+      duration: 1000,
+      backgroundColor: "green",
+      loaderCharacters: "*"
+    }
+  ]
+  return tripSetupOutput
+}
