03a18d272158ee54b02245072f05640bd789dbe8
PWR <pwrpntr@gmail.com>
2025-10-09 20:16:42 +0200

    client: fix merge error

--- packages/client/src/lib/components/Admin/index.ts
+++ packages/client/src/lib/components/Admin/index.ts
@@ -8,5 +8,4 @@ export { default as AdminTripTable } from "./AdminTripTable/AdminTripTable.svelt
 export { default as AdminPastTripTable } from "./AdminPastTripTable/AdminPastTripTable.svelte"
 export { default as AdminEventLog } from "./AdminEventLog/AdminEventLog.svelte"
 export { default as ProfitLossHistoryGraph } from "./ProfitLossHistoryGraph/ProfitLossHistoryGraph.svelte"
-export { default as SingleTripProfitLossGraph } from "./SingleTripProfitLossGraph/SingleTripProfitLossGraph.svelte"
 export { default as Outcome } from "./Outcome/Outcome.svelte"
