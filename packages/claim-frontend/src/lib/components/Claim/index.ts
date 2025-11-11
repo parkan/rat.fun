0e9969d86afcf09a4f56b93e095aed764220dc16
dk1a <dk1a@protonmail.com>
2025-11-12 00:48:06 +0300

    claim basic ui

--- packages/claim-frontend/src/lib/components/Claim/index.ts
+++ packages/claim-frontend/src/lib/components/Claim/index.ts
@@ -1,2 +1,3 @@
+export { default as Available } from "./Available/Available.svelte"
 export { default as ConnectWalletForm } from "./ConnectWalletForm/ConnectWalletForm.svelte"
 export { default as Done } from "./Done/Done.svelte"
