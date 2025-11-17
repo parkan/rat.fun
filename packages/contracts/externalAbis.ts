adf2e7490bec9e4806cbfb955761cc1364464c81
dk1a <dk1a@protonmail.com>
2025-11-17 14:29:35 +0300

    export test numeraire abi

--- packages/contracts/externalAbis.ts
+++ packages/contracts/externalAbis.ts
@@ -1,4 +1,5 @@
 import ERC20EquivalentExchangeAbi from "./out/ERC20EquivalentExchange.sol/ERC20EquivalentExchange.abi.json" with { type: "json" }
 import ERC20AirdropMerkleProofAbi from "./out/ERC20AirdropMerkleProof.sol/ERC20AirdropMerkleProof.abi.json" with { type: "json" }
+import TestNumeraireTokenAbi from "./out/TestNumeraireToken.sol/TestNumeraireToken.abi.json" with { type: "json" }
 
-export { ERC20EquivalentExchangeAbi, ERC20AirdropMerkleProofAbi }
+export { ERC20EquivalentExchangeAbi, ERC20AirdropMerkleProofAbi, TestNumeraireTokenAbi }
