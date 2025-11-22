a06254a992d6c2acb1e18426c1d31ed09ecdc526
PWR <pwrpntr@gmail.com>
2025-11-22 17:59:36 +0100

    clients: change RPC server for services

--- packages/exchange-frontend/src/lib/mud/extendedChainConfigs/extendedBase.ts
+++ packages/exchange-frontend/src/lib/mud/extendedChainConfigs/extendedBase.ts
@@ -1,15 +1,12 @@
 import { base as baseConfig } from "viem/chains"
 import { type MUDChain } from "@latticexyz/common/chains"
-import { PUBLIC_BASE_RPC_URL, PUBLIC_BASE_BUNDLER_URL } from "$env/static/public"
+import { PUBLIC_BASE_RPC_URL } from "$env/static/public"
 
 export const extendedBase = {
   ...baseConfig,
   rpcUrls: {
     default: {
       http: [PUBLIC_BASE_RPC_URL, ...baseConfig.rpcUrls.default.http]
-    },
-    bundler: {
-      http: [PUBLIC_BASE_BUNDLER_URL]
     }
   },
   indexerUrl: "https://base.rat-fun-indexer.com"
--- packages/exchange-frontend/src/lib/mud/extendedChainConfigs/extendedBaseSepolia.ts
+++ packages/exchange-frontend/src/lib/mud/extendedChainConfigs/extendedBaseSepolia.ts
@@ -1,15 +1,12 @@
 import { baseSepolia as baseSepoliaConfig } from "viem/chains"
 import { type MUDChain } from "@latticexyz/common/chains"
-import { PUBLIC_BASE_SEPOLIA_RPC_URL, PUBLIC_BASE_SEPOLIA_BUNDLER_URL } from "$env/static/public"
+import { PUBLIC_BASE_SEPOLIA_RPC_URL } from "$env/static/public"
 
 export const extendedBaseSepolia = {
   ...baseSepoliaConfig,
   rpcUrls: {
     default: {
       http: [PUBLIC_BASE_SEPOLIA_RPC_URL, ...baseSepoliaConfig.rpcUrls.default.http]
-    },
-    bundler: {
-      http: [PUBLIC_BASE_SEPOLIA_BUNDLER_URL]
     }
   },
   indexerUrl: "https://base-sepolia.rat-fun-indexer.com"
