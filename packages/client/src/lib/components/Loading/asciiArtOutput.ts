7066ba3765f4ae534913ce1517985d039b3131da
PWR <pwrpntr@gmail.com>
2025-12-08 13:31:36 +0100

    client: fix ascii art

--- packages/client/src/lib/components/Loading/asciiArtOutput.ts
+++ packages/client/src/lib/components/Loading/asciiArtOutput.ts
@@ -3,7 +3,7 @@ import { TerminalOutputUnit } from "$lib/modules/terminal-typer/types"
 export const asciiArtOutput: TerminalOutputUnit[] = [
   {
     type: "text",
-    content: " ____    ____  ______    _____  __ __  ____     ,     .",
+    content: " ____    ____  ______    _____  __ __  ____   ,     .",
     color: "white",
     backgroundColor: "black",
     duration: 0,
@@ -11,7 +11,7 @@ export const asciiArtOutput: TerminalOutputUnit[] = [
   },
   {
     type: "text",
-    content: "$    \\  /    $$      $  $     $$  $  $$    \\    (\\,;,/)",
+    content: "$    \\  /    $$      $  $     $$  $  $$    \\  (\\,;,/)",
     color: "white",
     backgroundColor: "black",
     duration: 0,
@@ -19,7 +19,7 @@ export const asciiArtOutput: TerminalOutputUnit[] = [
   },
   {
     type: "text",
-    content: "$  D  )$  o  $$      $  $   __$$  $  $$  _  $    (o o)\\//,",
+    content: "$  D  )$  o  $$      $  $   __$$  $  $$  _  $  (o o)\\//,",
     color: "white",
     backgroundColor: "black",
     duration: 0,
@@ -27,7 +27,7 @@ export const asciiArtOutput: TerminalOutputUnit[] = [
   },
   {
     type: "text",
-    content: "$    / $     $$_$  $_$  $  $_  $  $  $$  $  $     \\ /     \\,",
+    content: "$    / $     $$_$  $_$  $  $_  $  $  $$  $  $   \\ /     \\,",
     color: "white",
     backgroundColor: "black",
     duration: 0,
@@ -35,7 +35,7 @@ export const asciiArtOutput: TerminalOutputUnit[] = [
   },
   {
     type: "text",
-    content: "$    \\ $  _  $  $  $ __ $   _] $  :  $$  $  $     `+'(  (   \\    )",
+    content: "$    \\ $  _  $  $  $ __ $   _] $  :  $$  $  $   `+'(  (   \\   )",
     color: "white",
     backgroundColor: "black",
     duration: 0,
@@ -43,7 +43,7 @@ export const asciiArtOutput: TerminalOutputUnit[] = [
   },
   {
     type: "text",
-    content: "$  .  \\$  $  $  $  $$  $$  $   $     $$  $  $       //  \\   $_./",
+    content: "$  .  \\$  $  $  $  $$  $$  $   $     $$  $  $     //  \\   $_./",
     color: "white",
     backgroundColor: "black",
     duration: 0,
@@ -51,7 +51,7 @@ export const asciiArtOutput: TerminalOutputUnit[] = [
   },
   {
     type: "text",
-    content: "$__$\\_$$__$__$  $__$$__$$__$    \\__,_$$__$__$     '~' '~----'",
+    content: "$__$\\_$$__$__$  $__$$__$$__$    \\__,_$$__$__$   '~' '~----'",
     color: "white",
     backgroundColor: "black",
     duration: 0,
