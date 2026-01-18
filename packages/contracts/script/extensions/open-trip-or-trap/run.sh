083a6c2c053faa6dc21affe4552e7f11b2aa77db
PWR <pwrpntr@gmail.com>
2026-01-18 13:36:06 +0100

    contracts: deploy nft

--- packages/contracts/script/extensions/open-trip-or-trap/run.sh
+++ packages/contracts/script/extensions/open-trip-or-trap/run.sh
@@ -28,22 +28,24 @@ set -e
 # TESTNET (Base Sepolia)
 # # # # # # # # # # # #
 
-TESTNET_WORLD_ADDRESS=0xb559D9fb876F6fC3AC05B21004B33760B3582042
-TESTNET_RPC_URL=https://base-sepolia.g.alchemy.com/v2/xEQqRE27u_VVjfn5HlN2J
+# TESTNET_WORLD_ADDRESS=0xb559D9fb876F6fC3AC05B21004B33760B3582042
+# TESTNET_RPC_URL=https://base-sepolia.g.alchemy.com/v2/xEQqRE27u_VVjfn5HlN2J
 
 # forge script ./DeployOpenTripOrTrap.s.sol --sig run\(address\) $TESTNET_WORLD_ADDRESS --rpc-url $TESTNET_RPC_URL -vvv --broadcast
-forge script ./VerifyDeployment.s.sol --sig 'run(address)' $TESTNET_WORLD_ADDRESS --rpc-url $TESTNET_RPC_URL -vvv
+# forge script ./VerifyDeployment.s.sol --sig 'run(address)' $TESTNET_WORLD_ADDRESS --rpc-url $TESTNET_RPC_URL -vvv
 
 
 # # # # # # # # # # # #
 # MAINNET (Base Mainnet)
 # # # # # # # # # # # #
 
-# MAINNET_WORLD_ADDRESS=0x28d10E6aAb1a749Be792b4D8aa0519c70E83386a
+#MAINNET_WORLD_ADDRESS=0x28d10E6aAb1a749Be792b4D8aa0519c70E83386a
 # MAINNET_RPC_URL=https://base-mainnet.g.alchemy.com/v2/-hnbjcqjwXmO7ip5cyHBh
 
 # Full upgrade (tables + systems + init):
-# forge script ./DeployOpenTripOrTrap.s.sol --sig run\(address\) $MAINNET_WORLD_ADDRESS --rpc-url $MAINNET_RPC_URL --broadcast -vvv
+# forge script ./DeployOpenTripOrTrap.s.sol --sig run\(address\) $MAINNET_WORLD_ADDRESS --rpc-url $MAINNET_RPC_URL -vvv --broadcast
+# forge script ./VerifyDeployment.s.sol --sig 'run(address)' $MAINNET_WORLD_ADDRESS --rpc-url $MAINNET_RPC_URL -vvv
+
 
 # # # # # # # # # # # #
 # REGISTER TABLES ONLY
--- packages/contracts/script/itemNft/run.sh
+++ packages/contracts/script/itemNft/run.sh
@@ -23,20 +23,21 @@ set -e
 # TESTNET (Base Sepolia)
 # # # # # # # # # # # #
 
-TESTNET_WORLD_ADDRESS=0xb559D9fb876F6fC3AC05B21004B33760B3582042
-TESTNET_RPC_URL=https://base-sepolia.g.alchemy.com/v2/xEQqRE27u_VVjfn5HlN2J
+# TESTNET_WORLD_ADDRESS=0xb559D9fb876F6fC3AC05B21004B33760B3582042
+# TESTNET_RPC_URL=https://base-sepolia.g.alchemy.com/v2/xEQqRE27u_VVjfn5HlN2J
 
 # Full deploy (first time):
 # forge script ./DeployItemNFT.s.sol --sig 'run(address)' $TESTNET_WORLD_ADDRESS --rpc-url $TESTNET_RPC_URL -vvv --broadcast
 
 # Upgrade NFT contract only (keeps system, updates config):
-forge script ./UpgradeItemNFT.s.sol --sig 'run(address)' $TESTNET_WORLD_ADDRESS --rpc-url $TESTNET_RPC_URL -vvv --broadcast
+# forge script ./UpgradeItemNFT.s.sol --sig 'run(address)' $TESTNET_WORLD_ADDRESS --rpc-url $TESTNET_RPC_URL -vvv --broadcast
 
 # # # # # # # # # # # #
 # MAINNET (Base Mainnet)
 # # # # # # # # # # # #
 
 # MAINNET_WORLD_ADDRESS=0x28d10E6aAb1a749Be792b4D8aa0519c70E83386a
-# MAINNET_RPC_URL=https://base-mainnet.g.alchemy.com/v2/-hnbjcqjwXmO7ip5cyHBh
+#MAINNET_RPC_URL=https://base-mainnet.g.alchemy.com/v2/-hnbjcqjwXmO7ip5cyHBh
 
-#forge script ./DeployItemNFT.s.sol --sig 'run(address)' $MAINNET_WORLD_ADDRESS --rpc-url $MAINNET_RPC_URL --broadcast -vvv
+# forge script ./DeployItemNFT.s.sol --sig 'run(address)' $MAINNET_WORLD_ADDRESS --rpc-url $MAINNET_RPC_URL -vvv --broadcast
+
